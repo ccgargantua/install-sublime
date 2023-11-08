7f83913691545457557892f223d370e69a92f388
ccgargantua <lovelygargantua@gmail.com>
2023-11-08 11:07:21 -0500

    Don't reopen last session

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
-STR=$'{\n"hot_exit": "disabled",\n"translate_tabs_to_spaces": true,\n}'
+STR=$'{\n"hot_exit": "disabled",\n"remember_open_files": false,\n"translate_tabs_to_spaces": true,\n}'
 echo "$STR" >> temp.txt
 mv temp.txt ~/.config/sublime-text/Packages/User/Preferences.sublime-settings
